bc897229d1237bf063029f361177b82d56ee114d
Blazej Leman <blazejleman789@gmail.com>
2023-11-08 22:40:46 +0100

    przechodzenie do 10 miejsca w list

--- list.cpp
+++ list.cpp
@@ -6,5 +6,10 @@ int main() {
     std::list<int> list {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     std::cout << list.front() << std::endl;
     std::cout << list.back() << std::endl;
+    auto it = list.begin();
+    for (size_t i = 0; i < 9; i++){
+        ++it;
+    }
+    std::cout << *it << std::endl;
     return 0;
 }
